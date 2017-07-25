6886a08c7d4fde90b48b5e5ded67c53ae2d97242
Sambit Kumar Dash <sambitdash@gmail.com>
2017-07-25 15:37:56 +0530

    extract embedded file.
    This will not work for file attachments.

--- test/helpers.jl
+++ test/helpers.jl
@@ -33,3 +33,48 @@ function pdfhlp_extract_doc_content_to_dir(filename,dir=tempdir())
     pdDocClose(doc)
   end
 end
+
+function pdfhlp_extract_doc_embedded_files(filename,dir=tempdir())
+  file=rsplit(filename, '/',limit=2)
+  filenm=file[end]
+  dirpath=joinpath(dir,filenm)
+  if isdir(dirpath)
+    rm(dirpath; force=true, recursive=true)
+  end
+  mkdir(dirpath)
+  doc=pdDocOpen(filename)
+  try
+    catalog = pdDocGetCatalog(doc)
+    names = get(catalog, CosName("Names"))
+    cosDoc = pdDocGetCosDoc(doc)
+    nmdict = cosDocGetObject(cosDoc, names)
+    println(nmdict)
+    if nmdict !== CosNull
+      efref  = get(nmdict, CosName("EmbeddedFiles"))
+      efroot = cosDocGetObject(cosDoc, efref)
+      #simple case no tree just a few files attached in the root node.
+      #A proper implementation needs full names tree traversal.
+      efarr = get(efroot, CosName("Names"))
+      data = get(efarr)
+      len=length(data)
+      println(len)
+      for i=1:len:2
+        key=data[i]
+        println(key)
+        val=data[i+1]
+        filespec=cosDocGetObject(cosDoc, val)
+        ef=get(filespec, CosName("EF"))
+        filename=get(filespec,CosName("F")) #UF could be there as well.
+        stmref=get(ef, CosName("F"))
+        stm=cosDocGetObject(cosDoc,stmref)
+        bufstm=decode(stm)
+        buf=read(bufstm)
+        close(bufstm)
+        path=joinpath(dirpath,get(filename))
+        write(path,buf)
+      end
+    end
+  finally
+    pdDocClose(doc)
+  end
+end
